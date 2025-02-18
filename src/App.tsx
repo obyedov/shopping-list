27adbc7a2acdbf39a5600c48035e46e064314dc5
Vitaliy Obyedov <obyedov@gmail.com>
2025-02-18 22:28:50 +0100

    I added a buttons that filtered tasks.

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,11 @@ export type Task = {
 
 }
 
+export type FilterValues = 'all' | 'active' | 'completed'
+
 function App() {
+    const [filter, setFilter] = useState<FilterValues>('all')
+
     const [tasks, setTasks] = useState<Task[]>([
     { id: 1, title: 'Mela', completed: true },
     { id: 2, title: 'Banana', completed: true },
@@ -21,16 +25,34 @@ function App() {
  //     { id: 1, title: 'Farro perlato', completed: true },
   //    { id: 2, title: 'Crusca di avena', completed: false },
  // ]
+
     const deleteTask = (id: number) => {
+
         const filteredTasks = tasks.filter(task => {
             return task.id !== id
         })
         setTasks(filteredTasks)
     }
+    const changeFilter = (filter: FilterValues) => {
+        setFilter(filter)
+    }
+    let filteredTasks = tasks
+    if (filter === 'active') {
+        filteredTasks = tasks.filter(task => !task.completed)
+    }
+    if (filter === 'completed') {
+        filteredTasks = tasks.filter(task => task.completed)
+    }
 
   return (
     <>
-      <ShoppingList title ={"La frutta"} date="15.02.2025" tasks={tasks} deleteTask={deleteTask} />
+      <ShoppingList
+          title ={"La frutta"}
+          date="15.02.2025"
+
+          tasks={filteredTasks}
+          deleteTask={deleteTask}
+      changeFilter={changeFilter}/>
     </>
   )
 }
--- src/ShoppingList.tsx
+++ src/ShoppingList.tsx
@@ -1,4 +1,4 @@
-import {Task} from "./App.tsx";
+import {FilterValues, Task} from "./App.tsx";
 import {Button} from "./Button.tsx";
 
 type Props = {
@@ -6,9 +6,10 @@ type Props = {
     tasks: Task[]
     date?: string
     deleteTask: (id: number) => void
+    changeFilter: (filter: FilterValues) => void
 }
 export const ShoppingList = (props: Props) => {
-    const {title, tasks, date, deleteTask} = props
+    const {title, tasks, date, deleteTask, changeFilter} = props
     return (
         <div>
             <h2>{title}</h2>
@@ -32,6 +33,11 @@ export const ShoppingList = (props: Props) => {
 
             </ul>
                 )}
+            <div>
+                <Button title={'All'} onClick={() => changeFilter('all')}/>
+                <Button title={'Active'} onClick={() => changeFilter('active')} />
+                <Button title={'Completed'} onClick={() => changeFilter('completed')}/>
+            </div>
             <div>{date}</div>
 
         </div>
